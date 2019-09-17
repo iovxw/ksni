f88b00b30e7c37a3945f7a774fd814f28ec54188
iovxw <iovxw@outlook.com>
2019-09-17 23:44:27 +0800

    Move caches from State to TrayService

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,5 @@
-use std::collections::HashMap;
-use std::hash::{Hash, Hasher};
-use std::sync::atomic::{AtomicU32, Ordering};
-use std::sync::{mpsc, Arc, Mutex};
-
-use dbus::arg::{RefArg, Variant};
-use dbus::blocking::stdintf::org_freedesktop_dbus::PropertiesPropertiesChanged;
-use dbus::message::SignalArgs;
+use std::sync::atomic::AtomicBool;
+use std::sync::{Arc, Mutex};
 
 mod dbus_ext;
 mod dbus_interface;
@@ -14,11 +8,6 @@ pub mod menu;
 mod service;
 mod tray;
 
-use dbus_interface::{
-    DbusmenuItemsPropertiesUpdated, DbusmenuLayoutUpdated, StatusNotifierItemNewAttentionIcon,
-    StatusNotifierItemNewIcon, StatusNotifierItemNewOverlayIcon, StatusNotifierItemNewStatus,
-    StatusNotifierItemNewTitle, StatusNotifierItemNewToolTip,
-};
 pub use menu::{MenuItem, TextDirection};
 pub use service::TrayService;
 pub use tray::{Category, Icon, Status, ToolTip};
@@ -157,257 +146,26 @@ pub trait Tray: Sized {
 }
 
 pub struct State<T: ?Sized> {
-    tx: mpsc::Sender<dbus::Message>,
+    state_changed: Arc<AtomicBool>,
     inner: Arc<Mutex<T>>,
-    revision: Arc<AtomicU32>,
-    prop_cache: Arc<Mutex<PropertiesCache>>,
 }
 
 impl<T: Tray> State<T> {
-    pub fn update<F: Fn(&T)>(&self, f: F) {
-        let inner = self.inner.lock().unwrap();
-        (f)(&inner);
-        self.update_properties();
+    pub fn update<F: Fn(&mut T)>(&self, f: F) {
+        {
+            let mut inner = self.inner.lock().unwrap();
+            (f)(&mut inner);
+        }
+        //self.update_properties();
+        //self.update_menu();
     }
-
-    // TODO: macro?
-    fn update_properties(&self) {
-        dbg!("update");
-        let sni_dbus_path: dbus::Path = service::SNI_PATH.into();
-        let inner = self.inner.lock().unwrap();
-        let mut cache = self.prop_cache.lock().unwrap();
-        let mut dbusmenu_changed: HashMap<String, Variant<Box<dyn RefArg>>> = HashMap::new();
-        let mut sni_changed: HashMap<String, Variant<Box<dyn RefArg>>> = HashMap::new();
-
-        if let Some(text_direction) = cache.text_direction_changed(&*inner) {
-            dbusmenu_changed.insert(
-                "TextDirection".into(),
-                Variant(Box::new(text_direction.to_string())),
-            );
-        }
-
-        if let Some(tray_status) = cache.status_changed(&*inner) {
-            let msg = StatusNotifierItemNewStatus {
-                status: tray_status.to_string(),
-            }
-            .to_emit_message(&sni_dbus_path);
-            self.tx.send(msg).unwrap();
-            let menu_status = match tray_status {
-                tray::Status::Passive | tray::Status::Active => menu::Status::Normal,
-                tray::Status::NeedsAttention => menu::Status::Notice,
-            };
-            dbusmenu_changed.insert("Status".into(), Variant(Box::new(menu_status.to_string())));
-        }
-
-        if let Some(icon_theme_path) = cache.icon_theme_path_changed(&*inner) {
-            dbusmenu_changed.insert(
-                "IconThemePath".into(),
-                Variant(Box::new(icon_theme_path.to_string())),
-            );
-            sni_changed.insert(
-                "IconThemePath".into(),
-                Variant(Box::new(vec![icon_theme_path.to_string()])),
-            );
-        }
-
-        if !dbusmenu_changed.is_empty() {
-            let msg = PropertiesPropertiesChanged {
-                interface_name: "com.canonical.dbusmenu".to_owned(),
-                changed_properties: dbusmenu_changed,
-                invalidated_properties: Vec::new(),
-            }
-            .to_emit_message(&service::MENU_PATH.into());
-            self.tx.send(msg).unwrap();
-        }
-
-        if let Some(category) = cache.category_changed(&*inner) {
-            sni_changed.insert("Category".into(), Variant(Box::new(category.to_string())));
-        }
-
-        if let Some(window_id) = cache.window_id_changed(&*inner) {
-            sni_changed.insert("WindowId".into(), Variant(Box::new(window_id.to_string())));
-        }
-
-        if !sni_changed.is_empty() {
-            let msg = PropertiesPropertiesChanged {
-                interface_name: "org.kde.StatusNotifierItem".to_owned(),
-                changed_properties: sni_changed,
-                invalidated_properties: Vec::new(),
-            }
-            .to_emit_message(&sni_dbus_path);
-            self.tx.send(msg).unwrap();
-        }
-
-        // TODO: assert the id is consistent
-
-        if cache.title_changed(&*inner) {
-            let msg = StatusNotifierItemNewTitle {}.to_emit_message(&sni_dbus_path);
-            self.tx.send(msg).unwrap();
-        }
-        if cache.icon_changed(&*inner) {
-            let msg = StatusNotifierItemNewIcon {}.to_emit_message(&sni_dbus_path);
-            self.tx.send(msg).unwrap();
-        }
-        if cache.overlay_icon_changed(&*inner) {
-            let msg = StatusNotifierItemNewOverlayIcon {}.to_emit_message(&sni_dbus_path);
-            self.tx.send(msg).unwrap();
-        }
-        if cache.attention_icon_changed(&*inner) {
-            let msg = StatusNotifierItemNewAttentionIcon {}.to_emit_message(&sni_dbus_path);
-            self.tx.send(msg).unwrap();
-        }
-        if cache.tool_tip_changed(&*inner) {
-            let msg = StatusNotifierItemNewToolTip {}.to_emit_message(&sni_dbus_path);
-            self.tx.send(msg).unwrap();
-        }
-    }
-    fn update_menu(&self) -> bool {
-        let revision = self.revision.fetch_add(1, Ordering::AcqRel);
-        // TODO: check layout
-        let msg = DbusmenuLayoutUpdated {
-            parent: 0,
-            revision: revision,
-        }
-        .to_emit_message(&service::MENU_PATH.into());
-        self.tx.send(msg).unwrap();
-        // TODO: diff and send ItemsPropertiesUpdated
-        true
-    }
-}
-
-struct PropertiesCache {
-    category: Category,
-    title: u64,
-    status: Status,
-    window_id: i32,
-    icon_theme_path: u64,
-    icon: u64,
-    overlay_icon: u64,
-    attention_icon: u64,
-    tool_tip: u64,
-    text_direction: TextDirection,
-}
-
-impl PropertiesCache {
-    fn new<T: Tray>(tray: &T) -> Self {
-        PropertiesCache {
-            category: tray.category(),
-            title: hash_of(tray.title()),
-            status: tray.status(),
-            window_id: tray.window_id(),
-            icon_theme_path: hash_of(tray.icon_theme_path()),
-            icon: hash_of((tray.icon_name(), tray.icon_pixmap())),
-            overlay_icon: hash_of((tray.overlay_icon_name(), tray.overlay_icon_pixmap())),
-            attention_icon: hash_of((
-                tray.attention_icon_name(),
-                tray.attention_icon_pixmap(),
-                tray.attention_movie_name(),
-            )),
-            tool_tip: hash_of(tray.tool_tip()),
-            text_direction: tray.text_direction(),
-        }
-    }
-    fn category_changed<T: Tray>(&mut self, t: &T) -> Option<Category> {
-        let v = t.category();
-        if self.category != v {
-            self.category = v;
-            Some(v)
-        } else {
-            None
-        }
-    }
-    fn title_changed<T: Tray>(&mut self, t: &T) -> bool {
-        let hash = hash_of(t.title());
-        self.title != hash && {
-            self.title = hash;
-            true
-        }
-    }
-    fn status_changed<T: Tray>(&mut self, t: &T) -> Option<Status> {
-        let v = t.status();
-        if self.status != v {
-            self.status = v;
-            Some(v)
-        } else {
-            None
-        }
-    }
-    fn window_id_changed<T: Tray>(&mut self, t: &T) -> Option<i32> {
-        let v = t.window_id();
-        if self.window_id != v {
-            self.window_id = v;
-            Some(v)
-        } else {
-            None
-        }
-    }
-    fn icon_theme_path_changed<T: Tray>(&mut self, t: &T) -> Option<String> {
-        let v = t.icon_theme_path();
-        let hash = hash_of(&v);
-        if self.icon_theme_path != hash {
-            self.icon_theme_path = hash;
-            Some(v)
-        } else {
-            None
-        }
-    }
-    fn icon_changed<T: Tray>(&mut self, tray: &T) -> bool {
-        let hash = hash_of((tray.icon_name(), tray.icon_pixmap()));
-        self.icon != hash && {
-            self.icon = hash;
-            true
-        }
-    }
-    fn overlay_icon_changed<T: Tray>(&mut self, tray: &T) -> bool {
-        let hash = hash_of((tray.overlay_icon_name(), tray.overlay_icon_pixmap()));
-        self.overlay_icon != hash && {
-            self.overlay_icon = hash;
-            true
-        }
-    }
-    fn attention_icon_changed<T: Tray>(&mut self, tray: &T) -> bool {
-        let hash = hash_of((
-            tray.attention_icon_name(),
-            tray.attention_icon_pixmap(),
-            tray.attention_movie_name(),
-        ));
-        self.attention_icon != hash && {
-            self.attention_icon = hash;
-            true
-        }
-    }
-    fn tool_tip_changed<T: Tray>(&mut self, tray: &T) -> bool {
-        let hash = hash_of(tray.tool_tip());
-        self.tool_tip != hash && {
-            self.tool_tip = hash;
-            true
-        }
-    }
-    fn text_direction_changed<T: Tray>(&mut self, t: &T) -> Option<TextDirection> {
-        let v = t.text_direction();
-        if self.text_direction != v {
-            self.text_direction = v;
-            Some(v)
-        } else {
-            None
-        }
-    }
-}
-
-fn hash_of<T: Hash>(v: T) -> u64 {
-    use std::collections::hash_map::DefaultHasher;
-    let mut hasher = DefaultHasher::new();
-    v.hash(&mut hasher);
-    hasher.finish()
 }
 
 impl<T> Clone for State<T> {
     fn clone(&self) -> Self {
         State {
-            tx: self.tx.clone(),
+            state_changed: self.state_changed.clone(),
             inner: self.inner.clone(),
-            revision: self.revision.clone(),
-            prop_cache: self.prop_cache.clone(),
         }
     }
 }
--- src/service.rs
+++ src/service.rs
@@ -1,48 +1,53 @@
+use std::cell::Cell;
 use std::cell::RefCell;
 use std::collections::HashMap;
 use std::fmt;
+use std::hash::{Hash, Hasher};
 use std::rc::Rc;
-use std::sync::atomic::{AtomicU32, AtomicUsize, Ordering};
+use std::sync::atomic::{AtomicBool, AtomicU32, AtomicUsize, Ordering};
 use std::sync::{mpsc, Arc, Mutex};
 use std::thread;
 use std::time::Duration;
 
 use dbus;
 use dbus::arg::{RefArg, Variant};
+use dbus::blocking::stdintf::org_freedesktop_dbus::PropertiesPropertiesChanged;
 use dbus::blocking::Connection;
 use dbus::channel::{MatchingReceiver, Sender};
-use dbus::message::{MatchRule, MessageType, SignalArgs};
+use dbus::message::SignalArgs;
+use dbus::message::{MatchRule, MessageType};
 
 use crate::dbus_ext;
 use crate::dbus_interface;
-use crate::dbus_interface::StatusNotifierWatcher;
+use crate::dbus_interface::{
+    DbusmenuItemsPropertiesUpdated, DbusmenuLayoutUpdated, StatusNotifierItemNewAttentionIcon,
+    StatusNotifierItemNewIcon, StatusNotifierItemNewOverlayIcon, StatusNotifierItemNewStatus,
+    StatusNotifierItemNewTitle, StatusNotifierItemNewToolTip, StatusNotifierWatcher,
+};
 use crate::freedesktop;
 use crate::menu;
 use crate::tray;
 use crate::{State, Tray};
 
-pub(crate) const SNI_PATH: &str = "/StatusNotifierItem";
-pub(crate) const MENU_PATH: &str = "/MenuBar";
+const SNI_PATH: &str = "/StatusNotifierItem";
+const MENU_PATH: &str = "/MenuBar";
 
 static COUNTER: AtomicUsize = AtomicUsize::new(1);
 
 pub struct TrayService<T> {
     state: State<T>,
-    rx: mpsc::Receiver<dbus::Message>,
+    state_changed: Arc<AtomicBool>,
 }
 
 impl<T: Tray + 'static> TrayService<T> {
     pub fn new(tray: T) -> Self {
-        let (tx, rx) = mpsc::channel();
-        let prop_cache = super::PropertiesCache::new(&tray);
+        let state_changed = Arc::new(AtomicBool::new(false));
         TrayService {
             state: State {
-                tx: tx,
+                state_changed: state_changed.clone(),
                 inner: Arc::new(Mutex::new(tray)),
-                revision: Arc::new(AtomicU32::new(0)),
-                prop_cache: Arc::new(Mutex::new(prop_cache)),
             },
-            rx,
+            state_changed,
         }
     }
     pub fn state(&self) -> State<T> {
@@ -57,15 +62,19 @@ impl<T: Tray + 'static> TrayService<T> {
         let mut conn = Connection::new_session()?;
         conn.request_name(&name, true, true, false)?;
 
-        let menu = {
+        let (menu_cache, prop_cache) = {
             let state = self.state.inner.lock().unwrap();
-            RefCell::new(menu::menu_flatten(T::menu(&*state)))
+            (
+                RefCell::new(menu::menu_flatten(T::menu(&*state))),
+                RefCell::new(PropertiesCache::new(&*state)),
+            )
         };
         let inner = Rc::new(Inner {
             state: self.state,
-            msgs: self.rx,
-            menu: menu,
-            menu_path: MENU_PATH.into(),
+            state_changed: self.state_changed,
+            menu_cache,
+            revision: Cell::new(0),
+            prop_cache,
         });
 
         let tray_service2 = inner.clone();
@@ -111,7 +120,7 @@ impl<T: Tray + 'static> TrayService<T> {
 
         loop {
             conn.process(Duration::from_millis(50))?;
-            inner.flush_msgs();
+            inner.update_state();
         }
     }
 
@@ -150,28 +159,131 @@ fn register_to_watcher(conn: &Connection, name: String) -> Result<(), dbus::Erro
 
 struct Inner<T: Tray> {
     state: State<T>,
-    msgs: mpsc::Receiver<dbus::Message>,
+    state_changed: Arc<AtomicBool>,
     // A list of menu item and it's submenu
-    menu: RefCell<Vec<(menu::RawMenuItem<T>, Vec<usize>)>>,
-    menu_path: dbus::Path<'static>,
+    menu_cache: RefCell<Vec<(menu::RawMenuItem<T>, Vec<usize>)>>,
+    revision: Cell<u32>,
+    prop_cache: RefCell<PropertiesCache>,
 }
 
 impl<T: Tray + 'static> Inner<T> {
-    fn handle_updates(&self) {
-        dbg!("ahhh");
-        self.state.update_properties();
-        if self.state.update_menu() {
-            let state = self.state.inner.lock().unwrap();
-            *self.menu.borrow_mut() = menu::menu_flatten(T::menu(&*state));
+    fn update_state(&self) {
+        if self.state_changed.load(Ordering::Acquire) {
+            self.update_properties();
+            self.update_menu();
         }
-        self.flush_msgs();
     }
-    fn flush_msgs(&self) {
-        dbus_ext::with_current(|conn| {
-            while let Ok(msg) = self.msgs.try_recv() {
-                conn.send(msg).expect("send dbus message");
+    // TODO: macro?
+    fn update_properties(&self) {
+        let sni_dbus_path: dbus::Path = SNI_PATH.into();
+        let inner = self.state.inner.lock().unwrap();
+        let mut prop_cache = self.prop_cache.borrow_mut();
+        let mut dbusmenu_changed: HashMap<String, Variant<Box<dyn RefArg>>> = HashMap::new();
+        let mut sni_changed: HashMap<String, Variant<Box<dyn RefArg>>> = HashMap::new();
+
+        let mut dbus_msgs = Vec::new();
+
+        if let Some(text_direction) = prop_cache.text_direction_changed(&*inner) {
+            dbusmenu_changed.insert(
+                "TextDirection".into(),
+                Variant(Box::new(text_direction.to_string())),
+            );
+        }
+
+        if let Some(tray_status) = prop_cache.status_changed(&*inner) {
+            let msg = StatusNotifierItemNewStatus {
+                status: tray_status.to_string(),
+            }
+            .to_emit_message(&sni_dbus_path);
+            dbus_msgs.push(msg);
+            let menu_status = match tray_status {
+                tray::Status::Passive | tray::Status::Active => menu::Status::Normal,
+                tray::Status::NeedsAttention => menu::Status::Notice,
+            };
+            dbusmenu_changed.insert("Status".into(), Variant(Box::new(menu_status.to_string())));
+        }
+
+        if let Some(icon_theme_path) = prop_cache.icon_theme_path_changed(&*inner) {
+            dbusmenu_changed.insert(
+                "IconThemePath".into(),
+                Variant(Box::new(icon_theme_path.to_string())),
+            );
+            sni_changed.insert(
+                "IconThemePath".into(),
+                Variant(Box::new(vec![icon_theme_path.to_string()])),
+            );
+        }
+
+        if !dbusmenu_changed.is_empty() {
+            let msg = PropertiesPropertiesChanged {
+                interface_name: "com.canonical.dbusmenu".to_owned(),
+                changed_properties: dbusmenu_changed,
+                invalidated_properties: Vec::new(),
+            }
+            .to_emit_message(&MENU_PATH.into());
+            dbus_msgs.push(msg);
+        }
+
+        if let Some(category) = prop_cache.category_changed(&*inner) {
+            sni_changed.insert("Category".into(), Variant(Box::new(category.to_string())));
+        }
+
+        if let Some(window_id) = prop_cache.window_id_changed(&*inner) {
+            sni_changed.insert("WindowId".into(), Variant(Box::new(window_id.to_string())));
+        }
+
+        if !sni_changed.is_empty() {
+            let msg = PropertiesPropertiesChanged {
+                interface_name: "org.kde.StatusNotifierItem".to_owned(),
+                changed_properties: sni_changed,
+                invalidated_properties: Vec::new(),
+            }
+            .to_emit_message(&sni_dbus_path);
+            dbus_msgs.push(msg);
+        }
+
+        // TODO: assert the id is consistent
+
+        if prop_cache.title_changed(&*inner) {
+            let msg = StatusNotifierItemNewTitle {}.to_emit_message(&sni_dbus_path);
+            dbus_msgs.push(msg);
+        }
+        if prop_cache.icon_changed(&*inner) {
+            let msg = StatusNotifierItemNewIcon {}.to_emit_message(&sni_dbus_path);
+            dbus_msgs.push(msg);
+        }
+        if prop_cache.overlay_icon_changed(&*inner) {
+            let msg = StatusNotifierItemNewOverlayIcon {}.to_emit_message(&sni_dbus_path);
+            dbus_msgs.push(msg);
+        }
+        if prop_cache.attention_icon_changed(&*inner) {
+            let msg = StatusNotifierItemNewAttentionIcon {}.to_emit_message(&sni_dbus_path);
+            dbus_msgs.push(msg);
+        }
+        if prop_cache.tool_tip_changed(&*inner) {
+            let msg = StatusNotifierItemNewToolTip {}.to_emit_message(&sni_dbus_path);
+            dbus_msgs.push(msg);
+        }
+
+        dbus_ext::with_current(move |conn| {
+            for msg in dbus_msgs {
+                conn.send(msg).unwrap();
             }
         });
+    }
+    fn update_menu(&self) -> bool {
+        self.revision.set(self.revision.get() + 1);
+        // TODO: check layout
+        let msg = DbusmenuLayoutUpdated {
+            parent: 0,
+            revision: self.revision.get(),
+        }
+        .to_emit_message(&MENU_PATH.into());
+        dbus_ext::with_current(move |conn| {
+            conn.send(msg).unwrap();
+        });
+        // TODO: diff and send ItemsPropertiesUpdated
+        true
     }
 }
 
@@ -183,21 +295,24 @@ impl<T: Tray> fmt::Debug for Inner<T> {
 
 impl<T: Tray + 'static> dbus_interface::StatusNotifierItem for Inner<T> {
     fn activate(&self, x: i32, y: i32) -> Result<(), dbus::tree::MethodErr> {
-        let mut model = self.state.inner.lock().unwrap();
-        Tray::activate(&mut *model, x, y);
-        self.handle_updates();
+        self.state.update(|model| {
+            Tray::activate(model, x, y);
+        });
+        self.update_state();
         Ok(())
     }
     fn secondary_activate(&self, x: i32, y: i32) -> Result<(), dbus::tree::MethodErr> {
-        let mut model = self.state.inner.lock().unwrap();
-        Tray::secondary_activate(&mut *model, x, y);
-        self.handle_updates();
+        self.state.update(|model| {
+            Tray::secondary_activate(&mut *model, x, y);
+        });
+        self.update_state();
         Ok(())
     }
     fn scroll(&self, delta: i32, dir: &str) -> Result<(), dbus::tree::MethodErr> {
-        let mut model = self.state.inner.lock().unwrap();
-        Tray::scroll(&mut *model, delta, dir);
-        self.handle_updates();
+        self.state.update(|model| {
+            Tray::scroll(&mut *model, delta, dir);
+        });
+        self.update_state();
         Ok(())
     }
     fn context_menu(&self, _x: i32, _y: i32) -> Result<(), dbus::tree::MethodErr> {
@@ -295,10 +410,11 @@ impl<T: Tray + 'static> dbus_interface::Dbusmenu for Inner<T> {
         ),
         dbus::tree::MethodErr,
     > {
+        dbg!(parent_id, recursion_depth, &property_names);
         Ok((
-            self.state.revision.load(Ordering::Acquire),
+            self.revision.get(),
             crate::menu::to_dbusmenu_variant(
-                &self.menu.borrow(),
+                &self.menu_cache.borrow(),
                 parent_id as usize,
                 if recursion_depth < 0 {
                     None
@@ -322,7 +438,7 @@ impl<T: Tray + 'static> dbus_interface::Dbusmenu for Inner<T> {
             .map(|id| {
                 (
                     id,
-                    self.menu.borrow()[id as usize]
+                    self.menu_cache.borrow()[id as usize]
                         .0
                         .to_dbus_map(&property_names),
                 )
@@ -347,12 +463,11 @@ impl<T: Tray + 'static> dbus_interface::Dbusmenu for Inner<T> {
     ) -> Result<(), dbus::tree::MethodErr> {
         match event_id {
             "clicked" => {
-                let activate = self.menu.borrow()[id as usize].0.on_clicked.clone();
-                {
-                    let mut state = self.state.inner.lock().unwrap();
-                    (activate)(&mut state, id as usize);
-                }
-                self.handle_updates();
+                let activate = self.menu_cache.borrow()[id as usize].0.on_clicked.clone();
+                self.state.update(|state| {
+                    (activate)(state, id as usize);
+                });
+                self.update_state();
             }
             _ => (),
         }
@@ -364,7 +479,7 @@ impl<T: Tray + 'static> dbus_interface::Dbusmenu for Inner<T> {
     ) -> Result<Vec<i32>, dbus::tree::MethodErr> {
         let (found, not_found) = events
             .into_iter()
-            .partition::<Vec<_>, _>(|event| (event.0 as usize) < self.menu.borrow().len());
+            .partition::<Vec<_>, _>(|event| (event.0 as usize) < self.menu_cache.borrow().len());
         if found.is_empty() {
             return Err(dbus::tree::MethodErr::invalid_arg(
                 &"None of the id in the events can be found",
@@ -409,4 +524,130 @@ impl<T: Tray + 'static> dbus_interface::Dbusmenu for Inner<T> {
             vec![path]
         })
     }
+}
+
+struct PropertiesCache {
+    category: crate::Category,
+    title: u64,
+    status: crate::Status,
+    window_id: i32,
+    icon_theme_path: u64,
+    icon: u64,
+    overlay_icon: u64,
+    attention_icon: u64,
+    tool_tip: u64,
+    text_direction: crate::TextDirection,
+}
+
+impl PropertiesCache {
+    fn new<T: Tray>(tray: &T) -> Self {
+        PropertiesCache {
+            category: tray.category(),
+            title: hash_of(tray.title()),
+            status: tray.status(),
+            window_id: tray.window_id(),
+            icon_theme_path: hash_of(tray.icon_theme_path()),
+            icon: hash_of((tray.icon_name(), tray.icon_pixmap())),
+            overlay_icon: hash_of((tray.overlay_icon_name(), tray.overlay_icon_pixmap())),
+            attention_icon: hash_of((
+                tray.attention_icon_name(),
+                tray.attention_icon_pixmap(),
+                tray.attention_movie_name(),
+            )),
+            tool_tip: hash_of(tray.tool_tip()),
+            text_direction: tray.text_direction(),
+        }
+    }
+    fn category_changed<T: Tray>(&mut self, t: &T) -> Option<crate::Category> {
+        let v = t.category();
+        if self.category != v {
+            self.category = v;
+            Some(v)
+        } else {
+            None
+        }
+    }
+    fn title_changed<T: Tray>(&mut self, t: &T) -> bool {
+        let hash = hash_of(t.title());
+        self.title != hash && {
+            self.title = hash;
+            true
+        }
+    }
+    fn status_changed<T: Tray>(&mut self, t: &T) -> Option<crate::Status> {
+        let v = t.status();
+        if self.status != v {
+            self.status = v;
+            Some(v)
+        } else {
+            None
+        }
+    }
+    fn window_id_changed<T: Tray>(&mut self, t: &T) -> Option<i32> {
+        let v = t.window_id();
+        if self.window_id != v {
+            self.window_id = v;
+            Some(v)
+        } else {
+            None
+        }
+    }
+    fn icon_theme_path_changed<T: Tray>(&mut self, t: &T) -> Option<String> {
+        let v = t.icon_theme_path();
+        let hash = hash_of(&v);
+        if self.icon_theme_path != hash {
+            self.icon_theme_path = hash;
+            Some(v)
+        } else {
+            None
+        }
+    }
+    fn icon_changed<T: Tray>(&mut self, tray: &T) -> bool {
+        let hash = hash_of((tray.icon_name(), tray.icon_pixmap()));
+        self.icon != hash && {
+            self.icon = hash;
+            true
+        }
+    }
+    fn overlay_icon_changed<T: Tray>(&mut self, tray: &T) -> bool {
+        let hash = hash_of((tray.overlay_icon_name(), tray.overlay_icon_pixmap()));
+        self.overlay_icon != hash && {
+            self.overlay_icon = hash;
+            true
+        }
+    }
+    fn attention_icon_changed<T: Tray>(&mut self, tray: &T) -> bool {
+        let hash = hash_of((
+            tray.attention_icon_name(),
+            tray.attention_icon_pixmap(),
+            tray.attention_movie_name(),
+        ));
+        self.attention_icon != hash && {
+            self.attention_icon = hash;
+            true
+        }
+    }
+    fn tool_tip_changed<T: Tray>(&mut self, tray: &T) -> bool {
+        let hash = hash_of(tray.tool_tip());
+        self.tool_tip != hash && {
+            self.tool_tip = hash;
+            true
+        }
+    }
+    fn text_direction_changed<T: Tray>(&mut self, t: &T) -> Option<crate::TextDirection> {
+        let v = t.text_direction();
+        if self.text_direction != v {
+            self.text_direction = v;
+            Some(v)
+        } else {
+            None
+        }
+    }
+}
+
+fn hash_of<T: Hash>(v: T) -> u64 {
+    use std::collections::hash_map::DefaultHasher;
+    let mut hasher = DefaultHasher::new();
+    v.hash(&mut hasher);
+    hasher.finish()
 }

iovxw/ksni

commit 34e416115791b8829f974a20f91712d16375ff98

Add spawn_without_dbus_name

src/service.rs
@@ -17,18 +17,13 @@ use crate::dbus_interface::{
 use crate::menu;
 use crate::{Error, HandleReuest, OfflineReason, Tray};
 
-static COUNTER: AtomicUsize = AtomicUsize::new(1);
+static INSTANCE_COUNTER: AtomicUsize = AtomicUsize::new(1);
 
 pub(crate) async fn run<T: Tray>(
     service: Arc<Mutex<Service<T>>>,
     mut handle_rx: mpsc::UnboundedReceiver<HandleReuest>,
+    own_name: bool,
 ) -> Result<impl Future<Output = ()>, Error> {
-    let name = format!(
-        "org.kde.StatusNotifierItem-{}-{}",
-        std::process::id(),
-        COUNTER.fetch_add(1, Ordering::AcqRel)
-    );
-
     let sni_obj = StatusNotifierItem::new(service.clone());
     let menu_obj = DbusMenu::new(service.clone());
 
@@ -36,8 +31,6 @@ pub(crate) async fn run<T: Tray>(
     let conn = zbus::connection::Builder::session()
         .map_err(|e| Error::Dbus(e))?
         .internal_executor(false) // avoid extra thread when async-io enabled
-        .name(&*name)
-        .expect("validity of name should be ensured by developer")
         .serve_at(SNI_PATH, sni_obj)
         .expect("SNI_PATH should be valid")
         .serve_at(MENU_PATH, menu_obj)
@@ -45,6 +38,23 @@ pub(crate) async fn run<T: Tray>(
         .build()
         .await
         .map_err(|e| Error::Dbus(e))?;
+
+    let name = if own_name {
+        let name = format!(
+            "org.kde.StatusNotifierItem-{}-{}",
+            std::process::id(),
+            INSTANCE_COUNTER.fetch_add(1, Ordering::AcqRel)
+        );
+        conn.request_name(&*name).await.map_err(|e| {
+            assert_ne!(e, zbus::Error::NameTaken, "generated name should be unique");
+            Error::Dbus(e)
+        })?;
+        name
+    } else {
+        conn.unique_name()
+            .expect("unique name should be set after connected")
+            .to_string()
+    };
 
     if cfg!(feature = "async-io") {
         let executor = conn.executor().clone();

src/blocking.rs
@@ -12,9 +12,25 @@ use crate::{
 pub trait TrayMethods: Tray + private::Sealed {
     // TODO: doc
     fn spawn(self) -> Result<Handle<Self>, Error> {
+        self.spawn_with_name(true)
+    }
+
+    /// Run the tray service in background, but without a dbus well-known name
+    ///
+    /// This violates the [StatusNotifierItem] specification, but is required in some sandboxed
+    /// environments (e.g., flatpak).
+    ///
+    /// See <https://chromium-review.googlesource.com/c/chromium/src/+/4179380>
+    ///
+    /// [StatusNotifierItem]: https://www.freedesktop.org/wiki/Specifications/StatusNotifierItem/StatusNotifierItem/
+    fn spawn_without_dbus_name(self) -> Result<Handle<Self>, Error> {
+        self.spawn_with_name(false)
+    }
+    #[doc(hidden)]
+    fn spawn_with_name(self, own_name: bool) -> Result<Handle<Self>, Error> {
         let (handle_tx, handle_rx) = mpsc::unbounded_channel();
         let service = service::Service::new(self);
-        let service_loop = compat::block_on(service::run(service.clone(), handle_rx))?;
+        let service_loop = compat::block_on(service::run(service.clone(), handle_rx, own_name))?;
         thread::spawn(move || {
             compat::block_on(service_loop);
         });

src/lib.rs
@@ -223,6 +223,9 @@ pub enum Error {
 }
 
 // TODO: doc
+// the returned `Future` of all methods is always `Send`, because `Tray: Send` and `Self: Tray`
+// verified by `_assert_tray_methods_returned_future_is_send`
+#[allow(async_fn_in_trait)]
 pub trait TrayMethods: Tray + private::Sealed {
     // Get [`Handle`] of a running [`Tray`]
     //
@@ -235,14 +238,28 @@ pub trait TrayMethods: Tray + private::Sealed {
     //}
 
     // TODO: doc
-    #[allow(
-        async_fn_in_trait,
-        reason = "the returned `Future` of this method is always `Send`, because `Tray: Send` and `Self: Tray`"
-    )]
     async fn spawn(self) -> Result<Handle<Self>, Error> {
+        self.spawn_with_name(true).await
+    }
+
+    /// Run the tray service in background, but without a dbus well-known name
+    ///
+    /// This violates the [StatusNotifierItem] specification, but is required in some sandboxed
+    /// environments (e.g., flatpak).
+    ///
+    /// See <https://chromium-review.googlesource.com/c/chromium/src/+/4179380>
+    ///
+    /// [StatusNotifierItem]: https://www.freedesktop.org/wiki/Specifications/StatusNotifierItem/StatusNotifierItem/
+    async fn spawn_without_dbus_name(self) -> Result<Handle<Self>, Error> {
+        self.spawn_with_name(false).await
+    }
+
+    // sealed trait, safe to add private methods
+    #[doc(hidden)]
+    async fn spawn_with_name(self, own_name: bool) -> Result<Handle<Self>, Error> {
         let (handle_tx, handle_rx) = mpsc::unbounded_channel();
         let service = service::Service::new(self);
-        let service_loop = service::run(service.clone(), handle_rx).await?;
+        let service_loop = service::run(service.clone(), handle_rx, own_name).await?;
         compat::spawn(service_loop);
         Ok(Handle {
             service: Arc::downgrade(&service),
@@ -252,9 +269,10 @@ pub trait TrayMethods: Tray + private::Sealed {
 }
 impl<T: Tray> TrayMethods for T {}
 
-fn _assert_spawn_method_returned_future_is_send<T: Tray>(x: T) {
+fn _assert_tray_methods_returned_future_is_send<T: Tray + Clone>(x: T) {
     fn assert_send<T: Send>(_: T) {}
-    assert_send(x.spawn());
+    assert_send(x.clone().spawn());
+    assert_send(x.clone().spawn_without_dbus_name());
 }
 
 mod private {

commit 18f6622a8cd1e3dc10dbe524114abb7b96daff3c

Fix ksni::spawn hang under async-io feature
`spawn_without_dbus_name` is unaffected

Regression introduced in 34e416115791b8829f974a20f91712d16375ff98,
the new `request_name` method requires a working executor

src/service.rs
@@ -39,6 +39,17 @@ pub(crate) async fn run<T: Tray>(
         .await
         .map_err(|e| Error::Dbus(e))?;
 
+    if cfg!(feature = "async-io") {
+        let executor = conn.executor().clone();
+        // must start the executor before register_status_notifier_item
+        compat::spawn(async move {
+            // won't empty until conn stopped
+            while !executor.is_empty() {
+                executor.tick().await;
+            }
+        });
+    }
+
     let name = if own_name {
         let name = format!(
             "org.kde.StatusNotifierItem-{}-{}",
@@ -55,17 +66,6 @@ pub(crate) async fn run<T: Tray>(
             .expect("unique name should be set after connected")
             .to_string()
     };
-
-    if cfg!(feature = "async-io") {
-        let executor = conn.executor().clone();
-        // must start the executor before register_status_notifier_item
-        compat::spawn(async move {
-            // won't empty until conn stopped
-            while !executor.is_empty() {
-                executor.tick().await;
-            }
-        });
-    }
 
     let snw_object = StatusNotifierWatcherProxy::new(&conn)
         .await
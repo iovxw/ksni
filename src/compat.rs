849d43347787b70cb45cb391d0692f3e6030721b
iovxw <iovxw@outlook.com>
2026-06-06 17:18:34 +0800

    Move tests

--- src/compat.rs
+++ src/compat.rs
@@ -115,6 +115,74 @@ mod async_io {
         state.kick_driver();
     }
 
+    #[doc(hidden)]
+    #[macro_export]
+    macro_rules! select {
+        ($($patten:pat = $exp:expr => $blk:block)*) => {
+            futures_util::select! {
+                $( v = $exp => {
+                    let $patten = v else { continue };
+                    $blk
+                } )*
+            }
+        };
+    }
+    pub use crate::select;
+
+    pub mod mpsc {
+        use futures_util::StreamExt;
+
+        pub use futures_channel::mpsc::TrySendError as SendError;
+
+        pub fn unbounded_channel<T>() -> (UnboundedSender<T>, UnboundedReceiver<T>) {
+            let (tx, rx) = futures_channel::mpsc::unbounded();
+            (UnboundedSender(tx), UnboundedReceiver(rx))
+        }
+
+        pub struct UnboundedSender<T>(futures_channel::mpsc::UnboundedSender<T>);
+        impl<T> UnboundedSender<T> {
+            pub fn send(&self, value: T) -> Result<(), SendError<T>> {
+                self.0.unbounded_send(value)
+            }
+            pub fn is_closed(&self) -> bool {
+                self.0.is_closed()
+            }
+        }
+        impl<T> Clone for UnboundedSender<T> {
+            fn clone(&self) -> Self {
+                UnboundedSender(self.0.clone())
+            }
+        }
+
+        pub struct UnboundedReceiver<T>(futures_channel::mpsc::UnboundedReceiver<T>);
+        impl<T> UnboundedReceiver<T> {
+            pub fn recv(
+                &mut self,
+            ) -> futures_util::stream::Next<'_, futures_channel::mpsc::UnboundedReceiver<T>>
+            {
+                self.0.next()
+            }
+        }
+    }
+
+    pub mod oneshot {
+        pub use futures_channel::oneshot::{channel, Receiver, Sender};
+        //    use std::future::Future;
+        //
+        //    pub use async_channel::Sender;
+        //    pub fn channel<T>() -> (
+        //        Sender<T>,
+        //        Receiver<T>,
+        //    ) {
+        //        // The concurrent-queue that used by async-channel has
+        //        // single-capacity optimization, performace is fine
+        //        let (tx, rx) = async_channel::bounded(1);
+        //        let rx = async move { rx.recv().await };
+        //        (tx, rx)
+        //    }
+        //    pub type Receiver<T> = impl Future<Output = Result<T, async_channel::RecvError>>;
+    }
+
     #[cfg(test)]
     mod tests {
         use std::sync::mpsc;
@@ -237,74 +305,6 @@ mod async_io {
 
             std::panic::set_hook(hook);
         }
-    }
-
-    #[doc(hidden)]
-    #[macro_export]
-    macro_rules! select {
-        ($($patten:pat = $exp:expr => $blk:block)*) => {
-            futures_util::select! {
-                $( v = $exp => {
-                    let $patten = v else { continue };
-                    $blk
-                } )*
-            }
-        };
-    }
-    pub use crate::select;
-
-    pub mod mpsc {
-        use futures_util::StreamExt;
-
-        pub use futures_channel::mpsc::TrySendError as SendError;
-
-        pub fn unbounded_channel<T>() -> (UnboundedSender<T>, UnboundedReceiver<T>) {
-            let (tx, rx) = futures_channel::mpsc::unbounded();
-            (UnboundedSender(tx), UnboundedReceiver(rx))
-        }
-
-        pub struct UnboundedSender<T>(futures_channel::mpsc::UnboundedSender<T>);
-        impl<T> UnboundedSender<T> {
-            pub fn send(&self, value: T) -> Result<(), SendError<T>> {
-                self.0.unbounded_send(value)
-            }
-            pub fn is_closed(&self) -> bool {
-                self.0.is_closed()
-            }
-        }
-        impl<T> Clone for UnboundedSender<T> {
-            fn clone(&self) -> Self {
-                UnboundedSender(self.0.clone())
-            }
-        }
-
-        pub struct UnboundedReceiver<T>(futures_channel::mpsc::UnboundedReceiver<T>);
-        impl<T> UnboundedReceiver<T> {
-            pub fn recv(
-                &mut self,
-            ) -> futures_util::stream::Next<'_, futures_channel::mpsc::UnboundedReceiver<T>>
-            {
-                self.0.next()
-            }
-        }
-    }
-
-    pub mod oneshot {
-        pub use futures_channel::oneshot::{channel, Receiver, Sender};
-        //    use std::future::Future;
-        //
-        //    pub use async_channel::Sender;
-        //    pub fn channel<T>() -> (
-        //        Sender<T>,
-        //        Receiver<T>,
-        //    ) {
-        //        // The concurrent-queue that used by async-channel has
-        //        // single-capacity optimization, performace is fine
-        //        let (tx, rx) = async_channel::bounded(1);
-        //        let rx = async move { rx.recv().await };
-        //        (tx, rx)
-        //    }
-        //    pub type Receiver<T> = impl Future<Output = Result<T, async_channel::RecvError>>;
     }
 }
 #[cfg(all(feature = "async-io", not(feature = "tokio")))]
